616edb053c3f227c3f3cce76b3204ede3db8f454
Your Name <you@example.com>
2023-02-01 17:53:56 +0530

    f

--- src/DateDesign.js
+++ src/DateDesign.js
@@ -1,37 +1,11 @@
-import React from "react";
-import TextField from "@mui/material/TextField";
-import Box from "@mui/material/Box";
-import { LocalizationProvider } from "@mui/x-date-pickers-pro";
-import { AdapterDayjs } from "@mui/x-date-pickers-pro/AdapterDayjs";
-import { DateRangePicker } from "@mui/x-date-pickers-pro/DateRangePicker";
+import React from 'react'
 
 const DateDesign = () => {
-  const [value, setValue] = React.useState([null, null]);
-  
-  let d = value.toString()
-  console.log(d);
   return (
     <div>
-      <LocalizationProvider
-        dateAdapter={AdapterDayjs}
-        localeText={{ start: "Check-in", end: "Check-out" }}
-      >
-        <DateRangePicker
-          value={value}
-          onChange={(newValue) => {
-            setValue(newValue);
-          }}
-          renderInput={(startProps, endProps) => (
-            <React.Fragment>
-              <TextField {...startProps} />
-              <Box sx={{ mx: 2 }}> to </Box>
-              <TextField {...endProps} />
-            </React.Fragment>
-          )}
-        />
-      </LocalizationProvider>
+      
     </div>
-  );
-};
+  )
+}
 
-export default DateDesign;
+export default DateDesign
