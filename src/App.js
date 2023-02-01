a650cc0bbf4a800483747e7c9a49b7564a52b43c
Your Name <you@example.com>
2023-02-01 09:40:24 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [Incrise, SetIncrise] = useState(0);
+  let updateCounter = () => {
+    SetIncrise(Incrise + 1);
+  };
+  let DecriseCounter = () => {
+    SetIncrise(Incrise - 1);
+  };
+  let handelchange = (e) => {
+    SetIncrise(e.target.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello, world!</h1>
+      <button onClick={updateCounter}>incrise</button>
+      <h1 onChange={handelchange}>{Incrise}</h1>
+      <button onClick={DecriseCounter}>decrise</button>
     </div>
   );
-}
+};
 
 export default App;
